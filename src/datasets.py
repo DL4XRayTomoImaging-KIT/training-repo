35cd4992e7eff031a019fe562dad380a54292f9b
arinaruck <arina.ruck@gmail.com>
2021-06-21 23:13:27 +0200

    disjoin organs and tumors

--- src/datasets.py
+++ src/datasets.py
@@ -1,5 +1,5 @@
 from torch.utils.data import ConcatDataset, DataLoader, Dataset, Subset
-from TVSD import VolumeSlicingDataset, ExpandedPaddedSegmentation
+from src.TVSD import VolumeSlicingDataset, ExpandedPaddedSegmentation
 from sklearn.model_selection import train_test_split
 import torch
 import numpy as np
@@ -13,26 +13,26 @@ import os
 import tifffile
 from tqdm import tqdm
 from joblib import Parallel, delayed
+from collections import Counter
+
+
+def gather_paths(path):
+    volumes, targets = [], []
+    with open(path) as f:
+        data_addresses = map(lambda x: x.strip('\n').split('     '), f.readlines())
+    for (v, t) in data_addresses:
+        volumes.append(v)
+        targets.append(t)
+    return volumes, targets
 
 
 def convert_target(addr, converter):
     if isinstance(list(converter.keys())[0], str):
         # this is because of the restrictions in the OmegaConf. Should be resolved with 2.1 version.
-        converter = {int(k): v for k, v in converter.items()}
-    markup = ExpandedPaddedSegmentation(addr)
-    markup.data = np.vectorize(converter.get)(markup.data)
-    return markup
-
-
-def supervised_segmentation_target_matcher(volumes, targets):
-    label_ids = [os.path.basename(i).split('.')[-2] for i in glob(targets.format('*'))]
-
-    if '-' in label_ids[0]:
-        volume_ids = [i.split('-')[1] for i in label_ids]
-    else:
-        volume_ids = label_ids
-
-    return [volumes.format(i) for i in volume_ids], [targets.format(i) for i in label_ids]
+        converter = {int(k): v for k,v in converter.items()}
+    volume = medload(addr)[0]
+    volume = np.vectorize(converter.get)(volume)
+    return volume
 
 
 def sklearn_train_test_split(gathered_data, random_state=None, train_volumes=None, volumes_limit=None):
@@ -51,8 +51,7 @@ def get_TVSD_datasets(data_addresses, aug=None, label_converter=None, n_procs=1,
             label = ExpandedPaddedSegmentation(label_addr)
 
         one_volume = VolumeSlicingDataset(image_addr, segmentation=label, augmentations=aug, **kwargs)
-        one_volume.task = name[0] if (name := image_addr.split('/')[-3]) != 'origin' else image_addr.split('/')[-4][0]
-        one_volume.name = image_addr[len('/mnt/data/machine-learning/arina/mots/'):]
+        one_volume.task = int(name[0]) if (name := image_addr.split('/')[-3]) != 'origin' else int(image_addr.split('/')[-4][0])
         return one_volume
 
     datasets = Parallel(n_jobs=n_procs, verbose=5)(delayed(_process_onevolume)
@@ -88,3 +87,78 @@ def TVSD_dataset_resample(dataset, segmented_part=1.0, empty_part=0.1):
     empty_subsample = adaptive_choice(np.where(1 - is_marked)[0], empty_part)
 
     return Subset(dataset, np.concatenate([segmented_subsample, empty_subsample]))
+
+
+def TaskWrapper_balanced_resample(dataset, upsample=5):
+    tasks = dataset.tasks
+    task_cnt = Counter(tasks)
+    weights = np.array([1 / task_cnt[task] for task in tasks])
+    weights /= weights.sum()
+    n = len(dataset)
+    idx = np.random.choice(n, n * upsample, replace=True, p=weights)
+    return Subset(dataset, idx)
+
+
+class TVSDTaskWrapper(Dataset):
+    def __init__(self, base_dataset, transform=lambda x: x, downsample_lbl=False, scale_factor_lbl=0.125):
+        super().__init__()
+        self.base_dataset = base_dataset
+        self.transform = transform
+        self.downsample_lbl = downsample_lbl
+        self.tasks = []
+
+        for dataset in self.base_dataset.dataset.datasets:
+            self.tasks.extend([dataset.task] * len(dataset))
+        self.tasks = np.array(self.tasks)[self.base_dataset.indices]
+
+        if isinstance(scale_factor_lbl, float):
+            self.scale_factor_lbl = (scale_factor_lbl, scale_factor_lbl)
+        else:
+            self.scale_factor_lbl = scale_factor_lbl
+
+    def __len__(self):
+        return len(self.base_dataset)
+
+    def __getitem__(self, index):
+        image, mask = self.base_dataset.__getitem__(index)
+        task = self.tasks[index]
+
+        sample = self.transform(deepcopy({'image': image, 'mask': mask}))
+
+        if self.downsample_lbl:  # Downsample
+            sample['mask'] = interpolate(sample['mask'][None, None, :, :].float(),
+                                            scale_factor=self.scale_factor_lbl, mode='nearest').squeeze().long()
+
+        sample['mask'] = self.id2trainId(sample['mask'], task)
+        return sample['image'], sample['mask'], task
+
+    def id2trainId(self, label, task_id):
+        if task_id in [0, 1, 3]:
+            organ = (label == 1)
+            tumor = (label == 2)
+        elif task_id == 2:
+            organ = (label == 1)
+            tumor = (label == 2)
+        elif task_id in [4, 5]:
+            organ = np.zeros_like(label)
+            tumor = (label == 1)
+        elif task_id == 6:
+            organ = (label == 1)
+            tumor = np.zeros_like(label)
+        else:
+            print("Error, No such task!")
+            return None
+
+        shape = label.shape
+        results_map = np.zeros((2, shape[1], shape[2])).astype(np.float32)
+
+        results_map[0, :, :] = np.where(organ, 1, 0)
+        results_map[1, :, :] = np.where(tumor, 1, 0)
+        return results_map
+
+
+def TVSD_rebalance(dataset, upsample=2):
+    tvsd_resampled_empty = TVSD_dataset_resample(dataset)
+    resampled_tasks = TVSDTaskWrapper(tvsd_resampled_empty)
+    balanced_tasks = TaskWrapper_balanced_resample(resampled_tasks, upsample=upsample)
+    return balanced_tasks
