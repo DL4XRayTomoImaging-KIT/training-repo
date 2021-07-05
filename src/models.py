5f43f698f49ac7a3c29f533982856c7c31367558
Yaroslav Zharov <mart.slaaf@gmail.com>
2021-07-05 15:38:21 +0200

    [+] testing config and model modifications

--- src/models.py
+++ src/models.py
@@ -174,14 +174,16 @@ class EUnet(Unet):
             else:
                 self.metric_head = nn.Conv2d(in_channels=embedding_depth, out_channels=embedding_depth, kernel_size=1)
         else:
-            self.metric_head = lambda x: x
+            self.metric_head = None
     
     def forward(self, *args, **kwargs):
         e, b_ = super().forward(*args, **kwargs)
         c = self.classification_head(e)
-        m = self.metric_head(e)
-        
-        return m, c
+        if self.metric_head is not None:
+            m = self.metric_head(e)
+            return m, c
+        else:
+            return c
 
 def get_recommended_batch_size(parameters_count, image_side):
     base_batch_size = 1
@@ -189,14 +191,22 @@ def get_recommended_batch_size(parameters_count, image_side):
     base_batch_size *= max(torch.cuda.device_count(), 1) # correction for the devices count
     base_batch_size = int(base_batch_size * (40_000_000 / parameters_count)) # correction for the model size
 
-def partial_load(model, state_dict, renew_parameters=None):
+def partial_load(model, state_dict, renew_parameters=None, drop_parameters=None):
     mp = set(model.state_dict().keys())
+    dp = set(state_dict.keys())
+
+    if drop_parameters is not None:
+        keys_to_pop = []
+        for parname in drop_parameters:
+            keys_to_pop += [k for k in dp if k.startswith(parname)]
+    for key in keys_to_pop:
+        state_dict.pop(key)
     dp = set(state_dict.keys())
 
     not_updated = list(mp-dp)
     if renew_parameters is not None:
         not_updated += renew_parameters
-
     model_side_addendum = {k:v for k,v in model.state_dict().items() if k in not_updated}
     state_dict.update(model_side_addendum)
+    
     model.load_state_dict(state_dict)
