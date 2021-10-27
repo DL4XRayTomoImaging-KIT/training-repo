f8108e4e3538a9cecdbd139501ada63657bd81bd
Yaroslav Zharov <mart.slaaf@gmail.com>
2021-10-27 15:41:24 +0200

    [+] Added PFPN network for pre-training causes

--- src/models.py
+++ src/models.py
@@ -3,8 +3,11 @@ from segmentation_models_pytorch import * # we need everything from the smp
 # now we define our own models
 import torch
 from torch import nn
-from torchvision.models.resnet import conv1x1, conv3x3, BasicBlock
+import torch.nn.functional as F
+from torchvision.models.resnet import conv1x1, conv3x3, BasicBlock, Bottleneck
 from einops import rearrange
+
+import numpy as np
 
 class MyPersonalResnet(nn.Module):
     def __init__(self, layers=[2,2,2,2], block=BasicBlock, num_classes=1000, zero_init_residual=False,
@@ -152,6 +155,88 @@ def mask_rcnn(encoder_name, encoder_weights, in_channels, classes):
 
     return model
 
+
+import fvcore.nn.weight_init as weight_init
+from detectron2.layers import Conv2d, ShapeSpec
+
+class MyPersonalSemSegFPNHead(nn.Module):
+    def __init__(self, in_features_names, feature_strides, feature_channels, num_classes):
+        super().__init__()
+
+        self.in_features      = in_features_names
+        self.ignore_value     = 255
+        num_classes           = num_classes
+        conv_dims             = 128
+        self.common_stride    = 4
+        norm                  = "GN"
+
+        self.scale_heads = []
+        for in_feature in self.in_features:
+            head_ops = []
+            head_length = max(
+                1, int(np.log2(feature_strides[in_feature]) - np.log2(self.common_stride))
+            )
+            for k in range(head_length):
+                norm_module = nn.GroupNorm(32, conv_dims) if norm == "GN" else None
+                conv = Conv2d(
+                    feature_channels[in_feature] if k == 0 else conv_dims,
+                    conv_dims,
+                    kernel_size=3,
+                    stride=1,
+                    padding=1,
+                    bias=not norm,
+                    norm=norm_module,
+                    activation=F.relu,
+                )
+                weight_init.c2_msra_fill(conv)
+                head_ops.append(conv)
+                if feature_strides[in_feature] != self.common_stride:
+                    head_ops.append(
+                        nn.Upsample(scale_factor=2, mode="bilinear", align_corners=False)
+                    )
+            self.scale_heads.append(nn.Sequential(*head_ops))
+            # self.add_module(in_feature, self.scale_heads[-1])
+            self.scale_heads = nn.ModuleList(self.scale_heads)
+        self.predictor = Conv2d(conv_dims, num_classes, kernel_size=1, stride=1, padding=0)
+        weight_init.c2_msra_fill(self.predictor)
+
+    def forward(self, features):
+        for i, f in enumerate(self.in_features):
+            if i == 0:
+                x = self.scale_heads[i](features[f])
+            else:
+                x = x + self.scale_heads[i](features[f])
+        x = self.predictor(x)
+        x = F.interpolate(
+                x, scale_factor=self.common_stride, mode="bilinear", align_corners=False
+            )
+        return x
+
+RESNET_PARAMETERS_DICT = {'resnet50': {'block': Bottleneck, 'layers': [3, 4, 6, 3]}, 'resnet18': {'block': BasicBlock, 'layers': [2, 2, 2, 2]}}
+FPN_PARAMETERS_DICT = {'resnet50': {'in_channels_list': [64, 256, 512, 1024, 2048]}, 'resnet18': {'in_channels_list': [64, 64, 128, 256, 512]}}
+
+class PFPN(nn.Module):
+    def __init__(self, in_channels, out_channels, num_classes_cls, backbone='resnet50', backbone_checkpoint=None, freeze_backbone=False):
+        super().__init__()
+        model = MyPersonalResnet(num_classes=num_classes_cls, in_channels=in_channels, **RESNET_PARAMETERS_DICT[backbone])
+        self.freeze_backbone = freeze_backbone
+        if self.freeze_backbone:
+            for param in model.parameters():
+                param.requires_grad = False
+        if backbone_checkpoint:
+            state_dict = torch.load(backbone_checkpoint)['model_state_dict']
+            model.load_state_dict(state_dict, strict=False)
+        self.backbone = BackboneWithFPN(backbone=model, return_layers={'conv1': 'p2', 'layer1': 'p3', 'layer2': 'p4', 'layer3': 'p5', 'layer4': 'p6'}, out_channels=256, **FPN_PARAMETERS_DICT[backbone])
+
+        self.head = MyPersonalSemSegFPNHead(['p3', 'p4', 'p5', 'p6'], 
+                                    {'p3':4, 'p4':8, 'p5':16, 'p6':32}, 
+                                    {'p3':256, 'p4':256, 'p5':256, 'p6':256}, 
+                                    out_channels)
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.head(x)
+        return x
 
 class EUnet(Unet):
     def __init__(self, *args, embedding_depth=512, nonlinear_heads=False, metric_head=False, **kwargs):
