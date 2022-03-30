f4d6c4bd14052d493d5f4fb9e8b93cfae63ac04e
Yaroslav Zharov <mart.slaaf@gmail.com>
2022-03-30 12:42:57 +0200

    [+] Added model loading as it was tested in fine-tuning branches.

--- src/models.py
+++ src/models.py
@@ -12,14 +12,30 @@ def get_recommended_batch_size(parameters_count, image_side):
     base_batch_size *= max(torch.cuda.device_count(), 1) # correction for the devices count
     base_batch_size = int(base_batch_size * (40_000_000 / parameters_count)) # correction for the model size
 
-def partial_load(model, state_dict, renew_parameters=None):
+def partial_load(model, state_dict, renew_parameters=None, drop_parameters=None):
     mp = set(model.state_dict().keys())
+    dp = set(state_dict.keys())
+
+    if drop_parameters is not None:
+        keys_to_pop = []
+        for parname in drop_parameters:
+            keys_to_pop += [k for k in dp if k.startswith(parname)]
+        for key in keys_to_pop:
+            state_dict.pop(key)
     dp = set(state_dict.keys())
 
     not_updated = list(mp-dp)
     if renew_parameters is not None:
         not_updated += renew_parameters
-
     model_side_addendum = {k:v for k,v in model.state_dict().items() if k in not_updated}
     state_dict.update(model_side_addendum)
+
     model.load_state_dict(state_dict)
+
+def load_to_smp(model, state_dict, renew_parameters=None, drop_parameters=None):
+    if not('fc.weight' in state_dict.keys()):
+        state_dict['fc.weight'] = None
+    if not('fc.bias' in state_dict.keys()):
+        state_dict['fc.bias'] = None
+
+    partial_load(model, state_dict, renew_parameters, drop_parameters)
