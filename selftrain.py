f440f6051eadf6c708ac4ed1603ed8a36f19f788
er5241 <jwalin.bhatt@gmail.com>
2022-12-01 19:12:11 +0100

    added separate file for selftraining

---
+++ selftrain.py
@@ -0,0 +1,202 @@
+# imports
+import torch
+from torch.utils.data import DataLoader
+
+import torch
+
+from catalyst.loggers.wandb import WandbLogger
+
+import hydra
+from omegaconf import DictConfig, OmegaConf
+from src.hydra_helpers import cfg_ut, check_exclusion
+
+import src.augmentations
+import src.models
+import src.loaders
+import src.losses
+import src.callbacks
+import src.runners
+
+from time import time
+
+
+# definitions
+
+tn = lambda t: t.detach().cpu().numpy()
+
+get_files_by_ids = lambda ids, address: [address.format(i) for i in ids]
+
+@cfg_ut('model', 'initializing model')
+def get_model(network_type, *args, **kwargs):
+    model = getattr(src.models, network_type)(*args, **kwargs)
+    trainable_parameters = sum(p.numel() for p in model.parameters())
+    print(f'trainable parameters in model: {trainable_parameters}')
+    return model
+    
+@cfg_ut('model', 'initializing models')
+def get_models(**models_dict):
+    
+    models = {}
+    for model_type, model_kwargs in models_dict.items():
+      network_type = model_kwargs.pop('network_type')
+      model = getattr(src.models, network_type)(**model_kwargs)
+      models[model_type] = model
+      trainable_parameters = sum(p.numel() for p in model.parameters())
+      print(f'trainable parameters in {model_type} model: {trainable_parameters}')
+    return models
+
+@cfg_ut('checkpoint', 'loading checkpoint')
+def load_weights(model, checkpoint_addr=None,
+                 checkpoint_key=None, model_key=None,
+                 load_function=None, **load_function_params):
+
+    if checkpoint_addr is None:
+        return model
+
+    state_dict = torch.load(checkpoint_addr)
+
+    if checkpoint_key is not None:
+        state_dict = state_dict[checkpoint_key]
+    if model_key is not None:
+        model = getattr(model, model_key)
+
+    if load_function is None:
+        model.load_state_dict(state_dict)
+    else:
+        getattr(src.models, load_function)(model, state_dict, **load_function_params)
+        
+@cfg_ut('dataset', 'loading datasets')
+def get_loaders(loaders_function='generic_loaders', **kwargs):
+    return getattr(src.loaders, loaders_function)(**kwargs)
+
+@cfg_ut('optimizer', 'preparing optimizer')
+def get_optimizer(model, optimizer_name, optimizer_hyper_parameters=None, 
+                  submodel_to_optimize=None, parameters_to_include=None, parameters_to_exclude=None):
+    optimizer_hyper_parameters = optimizer_hyper_parameters or {}
+
+    if submodel_to_optimize is not None:
+        if isinstance(submodel_to_optimize, str):
+            model = model[submodel_to_optimize]
+    
+    if (parameters_to_include is None) and (parameters_to_exclude is None):
+        params = model.parameters()
+    else:
+        params = model.named_parameters()
+        if parameters_to_include is not None:
+            params = {n:p for n,p in params if n.startswith(tuple(parameters_to_include))}
+        if parameters_to_exclude is not None:
+            params = {n:p for n,p in params if not n.startswith(tuple(parameters_to_exclude))}
+        params = params.values()
+
+    optimizer = getattr(torch.optim, optimizer_name)(params, **optimizer_hyper_parameters)
+    optimizer.zero_grad()
+
+    return optimizer
+
+@cfg_ut('criterion', 'getting criterion')
+def get_criterion(criterion_name, criterion_hyper_parameters=None):
+    criterion_hyper_parameters = criterion_hyper_parameters or {}
+    criterion = getattr(src.losses, criterion_name)(**criterion_hyper_parameters)
+    return criterion
+
+@cfg_ut('logger', 'initializing logger')
+def get_logger(project_name, log_cfg, experiment_name=None):
+    if project_name is not None:
+        wanlogger = WandbLogger(project=project_name, name=experiment_name, config=log_cfg)
+        return {'wandb': wanlogger}
+    else:
+        return None
+
+@cfg_ut('callbacks', 'getting callbacks', force=True)
+def get_callbacks(*args):
+    callbacks = [] 
+    for callbac_config in args:
+        kwgs = (callbac_config['kwargs'] or {})if 'kwargs' in  callbac_config.keys() else {}
+        new_callback = getattr(src.callbacks, callbac_config['name'])(**kwgs)
+
+        if isinstance(new_callback, list):
+            callbacks += new_callback
+        else:
+            callbacks.append(new_callback)
+    
+    return callbacks
+
+@cfg_ut('runner', 'initializing runner', force=True)
+def get_runner(runner_name='KDRunner', runner_kwargs=None):
+    kwgs = {'input_key':"features", 'output_key': "logits", 'target_key': "targets", 'loss_key': "loss"}
+    runner_kwargs = runner_kwargs or {}
+    kwgs.update(runner_kwargs)
+    
+    # return getattr(src.runners, runner_name)(**kwgs)
+    return getattr(src.runners, runner_name)()
+
+@cfg_ut('training', 'starting overall training', force=True)
+def do_training(runner, model, criterion, optimizer, loaders, logger, callbacks, num_steps=None, seed=None, **kwargs):
+    torch.backends.cudnn.benchmark = True
+
+    train_stage_loaders, inference_stage_loaders = loaders
+
+    if (num_steps is not None) and ('num_epochs' in kwargs.keys()) and (kwargs['num_epochs'] is not None):
+        raise Exception('both `num_epochs` and `num_steps` should not be configured together!')
+    
+    if num_steps is not None:
+        kwargs['num_epochs'] = num_steps // len(train_stage_loaders['train'])
+
+    runner.train(
+        model=model,
+        criterion=criterion,
+        optimizer=optimizer,
+        loaders=train_stage_loaders,
+        # callbacks=callbacks,
+        loggers=logger,
+        seed=seed,
+        **kwargs)
+    
+    if (inference_stage_loaders is not None) and inference_stage_loaders:
+        assert len([i for i in inference_stage_loaders.keys() if i.startswith('train')]) == 0, 'There should be no train datasets on the last evaluation step!'
+        
+        if 'valid' not in inference_stage_loaders.keys():
+            inference_stage_loaders['valid'] = train_stage_loaders['valid']
+
+        kwargs['num_epochs'] = 1
+        kwargs['verbose'] = True
+        kwargs['logdir'] = kwargs['logdir']+'_inference'
+        runner.train(
+            model=model,
+            criterion=criterion,
+            loaders=inference_stage_loaders,
+            callbacks=callbacks,
+            seed=seed,
+            **kwargs)
+
+def get_seed(cfg):
+    if ('seed' in cfg.keys()) and (cfg['seed'] is not None):
+        return cfg['seed']
+    else:
+        return int(time())
+
+@hydra.main(config_path='training_configs', config_name="config")
+def overall_training(cfg : DictConfig) -> None:
+    excluded = check_exclusion(cfg=cfg)
+    if excluded:
+        print("this job is marked to be excluded!")
+        print(OmegaConf.to_yaml(cfg['model']))
+        print(OmegaConf.to_yaml(cfg['training']))
+    else:
+        seed = get_seed(cfg=cfg)
+        # model = get_model(cfg=cfg)
+        model = get_models(cfg=cfg)
+        # load_weights(model, cfg=cfg)
+        load_weights(model['teacher'], cfg=cfg)
+        load_weights(model['student'], cfg=cfg)
+        loaders = get_loaders(cfg=cfg, seed=seed)
+        # optimizer = get_optimizer(model, cfg=cfg)
+        optimizer = get_optimizer(model['student'], cfg=cfg)        
+        criterion = get_criterion(cfg=cfg)
+        logger = get_logger(cfg=cfg, log_cfg=cfg)
+        callbacks = get_callbacks(cfg=cfg)
+        runner = get_runner(cfg=cfg)
+        do_training(runner, model, criterion, optimizer, loaders, logger, callbacks, cfg=cfg, seed=seed)
+
+if __name__ == "__main__":
+    overall_training()
--- src/runners.py
+++ src/runners.py
@@ -1,6 +1,8 @@
 from catalyst.runners.runner import *
+from catalyst import metrics
 import numpy as np
 import torch
+import torch.nn as nn
 
 def iou(preds, labels, C, EMPTY=1., ignore=None, per_image=False):
     """
@@ -23,3 +25,162 @@ def iou(preds, labels, C, EMPTY=1., ignore=None, per_image=False):
         ious.append(iou)
     ious = [np.mean(iou) for iou in zip(*ious)] # mean accross images if per_image
     return 100 * np.array(ious)
+    
+    
+def get_iou(preds, labels, label_to_calculate=None):
+    C = preds.shape[1]
+    preds = torch.argmax(preds, 1)
+    if label_to_calculate is not None:
+        return iou(preds, labels[:, 0], [label_to_calculate,]).mean()
+    else:
+        return iou(preds, labels[:, 0], C)[1:].mean() # ignoring background label.
+        
+        
+#########################
+
+def dice(preds, labels, C, EMPTY=1., ignore=None, per_image=False):
+    """
+    Array of dice score for each (non ignored) class
+    """
+    if not per_image:
+        preds, labels = (preds,), (labels,)
+    dices = []
+    for pred, label in zip(preds, labels):
+        dice = []
+        ran = C if isinstance(C, list) else range(C)
+        for i in ran:
+            if i != ignore: # The ignored label is sometimes among predicted classes (ENet - CityScapes)
+                intersection = 2*((label == i) & (pred == i)).sum()
+                union = ((label == i) & (label != ignore)).sum() + ((pred == i) & (label != ignore)).sum()
+                if not union:
+                    dice.append(EMPTY)
+                else:
+                    dice.append(float(intersection) / float(union))
+        dices.append(dice)
+    dices = [np.mean(dice) for dice in zip(*dices)] # mean accross images if per_image
+    return 100 * np.array(dices)
+
+def get_dice(preds, labels, label_to_calculate=None):
+    C = preds.shape[1]
+    preds = torch.argmax(preds, 1)
+    if label_to_calculate is not None:
+        return dice(preds, labels[:, 0], [label_to_calculate,]).mean()
+    else:
+        return dice(preds, labels[:, 0], C)[1:].mean() # ignoring background label.
+
+def dice_callbacks(classes):
+    if isinstance(classes, ListConfig) or isinstance(classes, list):
+        classes = classes
+    else:
+        classes = range(1, classes)
+
+    callbacks = []
+    for l in classes:
+        metric = FunctionalBatchMetric(metric_key=f'dice-{l}', metric_fn=partial(get_dice, label_to_calculate=l))
+        callbacks.append(FunctionalBatchMetricCallback(metric=metric, input_key='logits', target_key='targets'))
+    return callbacks
+
+def mean_dice_callback():
+    metric = FunctionalBatchMetric(metric_key=f'mean-dice', metric_fn=get_dice)
+    return FunctionalBatchMetricCallback(metric=metric, input_key='logits', target_key='targets')
+    
+#########################
+    
+    
+class MyCustomRunner(Runner):
+    def on_loader_start(self, runner):
+        super().on_loader_start(runner)
+        self.meters = {
+            key: metrics.AdditiveMetric(compute_on_call=False)
+            for key in ["loss", "mean-iou", "dice"]
+        }
+        
+        
+    def handle_batch(self, batch):
+        imgs = batch[0]
+        label = batch[1]
+        self.model['teacher'].requires_grad = False
+        self.model['student'].requires_grad = True
+
+        teach_embeds = self.model['teacher'](imgs)
+        teach_embeds = teach_embeds.argmax(1).unsqueeze(1).float()
+        stud_embeds = self.model['student'](imgs)
+
+        loss = self.criterion(stud_embeds, teach_embeds)
+        
+                                                
+        mean_iou = get_iou(stud_embeds, label)
+        dice_score = get_dice(stud_embeds, label)
+        
+        self.batch_metrics.update({'loss': loss.item()})
+        
+        self.batch_metrics.update({'mean-iou': mean_iou})
+        self.batch_metrics.update({'dice': dice_score})
+        for key in ["loss", "mean-iou", "dice"]:
+          self.meters[key].update(self.batch_metrics[key], self.batch_size)
+
+    
+        if self.is_train_loader:
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            
+            
+    def on_loader_end(self, runner):
+        for key in ["loss", "mean-iou", "dice"]:
+            self.loader_metrics[key] = self.meters[key].compute()[0]
+        super().on_loader_end(runner)
+        
+        
+        
+class KDRunner(Runner):
+    def on_loader_start(self, runner):
+        super().on_loader_start(runner)
+        self.meters = {
+            key: metrics.AdditiveMetric(compute_on_call=False)
+            for key in ["seg_loss", "kd_loss", 'stud_loss', "mean-iou", "dice"]
+        }
+        
+        
+    def handle_batch(self, batch):
+        imgs = batch[0]
+        label = batch[1]
+        self.model['teacher'].requires_grad = False
+        self.model['student'].requires_grad = True
+
+        teach_embeds = self.model['teacher'](imgs)
+        stud_embeds = self.model['student'](imgs)
+        
+        
+        classification = self.criterion(stud_embeds, label)
+        alpha = 0.1
+        temp = 4
+        criterion_KD = nn.KLDivLoss(reduction='mean')
+        loss_tfkd = criterion_KD(torch.log_softmax(stud_embeds/temp, dim=1), torch.softmax(teach_embeds/temp, dim=1).detach()) * (temp**2)
+        loss = (1-alpha) * classification + alpha * loss_tfkd
+        
+                                                
+        mean_iou = get_iou(stud_embeds, label)
+        dice_score = get_dice(stud_embeds, label)
+        
+        self.batch_metrics.update({'loss': loss.item()})
+        self.batch_metrics.update({'seg_loss': classification.item()})
+        self.batch_metrics.update({'kd_loss': loss_tfkd.item()})
+        self.batch_metrics.update({'stud_loss': loss.item()})
+        
+        self.batch_metrics.update({'mean-iou': mean_iou})
+        self.batch_metrics.update({'dice': dice_score})
+        for key in ["seg_loss", "kd_loss", "stud_loss", "mean-iou", "dice"]:
+          self.meters[key].update(self.batch_metrics[key], self.batch_size)
+
+    
+        if self.is_train_loader:
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+            
+            
+    def on_loader_end(self, runner):
+        for key in ["seg_loss", "kd_loss", "stud_loss", "mean-iou", "dice"]:
+            self.loader_metrics[key] = self.meters[key].compute()[0]
+        super().on_loader_end(runner)                        
